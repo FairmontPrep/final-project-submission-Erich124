990fbb69b06a8800abee94d5bc13b3a720ae709b
Erich124 <ehu6688@gmail.com>
2025-05-21 09:08:04 -0700

    Progress check for final project
    Progress check for final project--Eric Hu

--- Client.java
+++ Client.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Client{
+    static int[][] A;
     public static void main(String[] args){
+        run();
+        }
+
+        public static void run() {
         ArrayList<ArrayList<Integer>> test_array_2 = new ArrayList<>();
         test_array_2.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 0, 0, 0, 0 )));
         test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0 )));
@@ -12,9 +17,32 @@ public class Client{
         test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 0, 0, 0, 0 )));
         test_array_2.add(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 2, 0, 0, 0 )));
         test_array_2.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 1, 1, 1 )));
-        for (ArrayList<Integer> row : test_array_2) {
-            System.out.println(row);
+        A = new int[test_array_2.size()][test_array_2.get(0).size()];
+        for (int i = 0; i < test_array_2.size(); i++) {
+            for (int j = 0; j < test_array_2.get(i).size(); j++) {
+                A[i][j] = test_array_2.get(i).get(j);
+            }
         }
 
+        ArrayList<String> path = findPath();
+        System.out.println(path);
+        printPathMap(path);
+        }
+
+        public static ArrayList<String> findPath() {
+        ArrayList<String> result = new ArrayList<>();
+        boolean[][] visited = new boolean[A.length][A[0].length];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                if (A[i][j] == 1) {
+                    ArrayList<String> path = new ArrayList<>();
+                    if (dfs(i, j, visited, path)) {
+                        return path;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
